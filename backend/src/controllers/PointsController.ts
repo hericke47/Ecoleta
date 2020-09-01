a48a57ce6692b6cfc651b7840a71439cdad29f6f
Herick Exterkoetter <herickherick47@gmail.com>
2020-09-01 20:27:05 -0300

    listando pontos de coleta inicial

--- backend/src/controllers/PointsController.ts
+++ backend/src/controllers/PointsController.ts
@@ -2,6 +2,18 @@ import { Request, Response } from 'express';
 import knex from '../database/connection';
 
 class PointsController {
+  async show(request: Request, response: Response) {
+    const { id } = request.params;
+
+    const point = await knex('points').where('id', id).first();
+
+    if(!point) {
+      return response.status(400).json({ message: 'Point not found.' });
+    }
+
+    return response.json(point);
+  }
+
   async create(request: Request, response: Response) {
     const {
       name,
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,18 +1,15 @@
 import express from 'express';
-import knex from './database/connection';
 
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
-const pointsController = new PointsController();
-const itemsController = new ItemsController();
-
-
-
 
 const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
 routes.get('/items', itemsController.index);
 
 routes.post('/points', pointsController.create);
+routes.get('/points/:id', pointsController.show);
 
 export default routes;
