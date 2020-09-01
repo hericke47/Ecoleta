67a9c46a23ce10b3e6bbfa767ead787ecb1c8f7f
Herick Exterkoetter <herickherick47@gmail.com>
2020-09-01 20:15:35 -0300

    abstraindo listagem de items

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,24 +2,16 @@ import express from 'express';
 import knex from './database/connection';
 
 import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
 const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+
 
 
 const routes = express.Router();
 
-routes.get('/items', async (request, response) => {
-  const items = await knex('items').select('*');
-
-  const serializedItems = items.map(item => {
-    return {
-      id: item.id,
-      title: item.title,
-      image_url: `http://localhost:3333/uploads/${item.image}`,
-    }
-  })
-
-  return response.json(serializedItems)
-});
+routes.get('/items', itemsController.index);
 
 routes.post('/points', pointsController.create);
 
