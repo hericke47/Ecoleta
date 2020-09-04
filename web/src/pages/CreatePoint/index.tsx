a09201f7174a8004a9eb3b35b8856390058266f6
Herick Exterkoetter <herickherick47@gmail.com>
2020-09-04 16:53:43 -0300

    redirecionando para tela inicial

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
 import { LeafletMouseEvent } from 'leaflet';
@@ -42,6 +42,7 @@ const CreatePoint = () => {
 
   const [selectedItems, setSelectedItems] = useState<number[]>([]);
 
+  const history = useHistory();
 
   useEffect(() => {
     api.get('items').then(response => {
@@ -119,6 +120,7 @@ const CreatePoint = () => {
     await api.post('points', data);
 
     alert('Ponto de coleta criado');
+    history.push('/');
   }
 
   function handleSelectItem(id: number) {
