3921c9886d2dec7d60df21c64a3aa25620202b1f
Herick Exterkoetter <herickherick47@gmail.com>
2020-09-03 22:52:32 -0300

    mapa adicionado

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer, Marker } from 'react-leaflet';
 
 import './CreatePoint.css';
 
@@ -62,6 +63,16 @@ const CreatePoint = () => {
             <h2>Endereços</h2>
             <span>Selecione o endereço no mapa</span>
           </legend>
+
+
+          <Map center={[-28.4706384, -49.0312173]}  zoom={15}>
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+            <Marker position={[-28.4706384, -49.0312173]} />
+          </Map>
 
           <div className="field-group">
             <div className="field">
