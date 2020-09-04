a3adc28717f4cebf217b52711ddc089bf8c34b07
Herick Exterkoetter <herickherick47@gmail.com>
2020-09-03 22:09:27 -0300

    css do app, e fontes roboto e ubuntu

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,21 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
-import Header from './Header';
 
 const App: React.FC = () => {
-  const [counter, setCounter] = useState(0);
-
-  function handleButtonClick() {
-    setCounter(counter + 1);
-  }
-
   return (
     <div>
-      <Header title="Hello World" />
-
-      <h1>{counter}</h1>
-      <button type="button" onClick={handleButtonClick}>Acrescentar</button>
+      <h1>adfadfaf</h1>
     </div>
   );
 }
